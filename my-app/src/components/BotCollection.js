57be45242b64f6587bbcbe888edb0b4d422a8e16
Chantale1 <162121631+Chantale1@users.noreply.github.com>
2024-05-13 21:04:27 +0300

    Update BotCollection.js

--- my-app/src/components/BotCollection.js
+++ my-app/src/components/BotCollection.js
@@ -1,30 +1,33 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection({ onAddingBot, botsData }) {
-    
-    
-    return (
-        <div>
-            <h1>My Bot Collection</h1>
-            <ul>
-                { botsData && botsData.map(bot => (
-                    <li key={bot.id}>
-                        <h2>{bot.name}</h2>
-                        <img src={bot.avatar_url} alt={bot.name} />
-                        <div>
-                            <p>Health: {bot.health}</p>
-                            <p>Attack: {bot.damage}</p>
-                            <p>Defense: {bot.armor}</p>
-                            <p>Type: {bot.bot_class}</p>
-                            <p>Created: {bot.created_at}</p>
-                            <p>Updated: {bot.updated_at}</p>
-                        </div>
-                        <button onClick={() => onAddingBot(bot)}>Deploy</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+const BotCollection = ({ bots, addBot }) => {
+  return (
+    <div className="container">
+      <h1>My Bot Collection</h1>
+      <div className="d-flex flex-row flex-wrap justify-content-start">
+        {bots.map(bot => (
+              <div className="p-2" key={bot.id} style={{ minWidth: "200px" }}>
+              <BotCard bot={bot} addBot={addBot} />
+        
+            <div className="card">
+              <img src={bot.avatar_url} className="card-img-top" alt={bot.name} />
+              <div className="card-body">
+                <h5 className="card-title">{bot.name}</h5>
+                <p className="card-text">Health: {bot.health}</p>
+                <p className="card-text">Attack: {bot.damage}</p>
+                <p className="card-text">Defense: {bot.armor}</p>
+                <p className="card-text">Type: {bot.bot_class}</p>
+                <p className="card-text">Created: {bot.created_at}</p>
+                <p className="card-text">Updated: {bot.updated_at}</p>
+                <button className="btn btn-primary" onClick={() => addBot(bot)}>Deploy</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default BotCollection;
