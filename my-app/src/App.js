affcc4e663488aa5f24e824348c83822e00ddd83
Chantale1 <162121631+Chantale1@users.noreply.github.com>
2024-05-13 21:07:13 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import BotCollection from './components/BotCollection'
-import './App.css';
-import MyBotArmy from './components/YourBotArmy';
+import React from "react";
+import BotPage from "./components/BotsPage";
+import "./App.css";
 
-function App() {
-  const [allBots, setAllBots] = useState([]);
-  const [chosenBots, setChosenBots] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/bots")
-      .then((response) => response.json())
-      .then((data) =>
-        setAllBots(data)
-      )
-      .catch((error) => console.error(error));
-  }, []);
-
-  function addBot(bot) {
-    if (!chosenBots.some((b) => b.id === bot.id)) {
-      setChosenBots([...chosenBots, bot]);
-    }
+function App() { 
+  
+    return (
+      <div className="App">
+        <BotPage />
+      </div>
+    );
   }
 
-  function removeBot(bot) {
-    const updatedBots = chosenBots.filter((b) => b.id !== bot.id);
-    setChosenBots(updatedBots);
-  }
-
-  function dischargeBot(bot) {
-    fetch(`http://localhost:3000/bots/${bot.id}`, { method: "DELETE" })
-      .then(() => {
-        const updatedAllBots = allBots.filter((b) => b.id !== bot.id);
-        setAllBots(updatedAllBots);
-        removeBot(bot);
-      })
-      .catch((error) => console.error(error));
-  }
-
-  return (
-    <div className="App">
-      <BotCollection allBots={allBots} onAddBot={addBot} />
-      <MyBotArmy chosenBots={chosenBots} onRemoveBot={removeBot} onDischargeBot={dischargeBot} />
-    </div>
-  );
-}
 
 export default App;
