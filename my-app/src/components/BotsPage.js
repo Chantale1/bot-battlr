08db74fc7f83f61d5bd657df1bc1f0afdac76a67
Chantale1 <162121631+Chantale1@users.noreply.github.com>
2024-05-13 21:01:29 +0300

    Update BotsPage.js

--- my-app/src/components/BotsPage.js
+++ my-app/src/components/BotsPage.js
@@ -1,54 +1,49 @@
 import React, { useState, useEffect } from "react";
-import MyBotArmy from "./YourBotArmy";
+import MyBotArmy from "./MyBotArmy";
+import BotSearch from "./BotSearch";
 import BotCollection from "./BotCollection";
+import BotSpecs from "./BotSpecs";
 
-function BotPage() {
-  const [botsData, setBotData] = useState([]);
-
-  function fetchData() {
-    return fetch("http://localhost:3000/bots")
-      .then((resp) => resp.json())
-      .then((data) => {
-        setBotsData(data);
-      });
-  }
+const BotsPage = () => {
+  const [allBots, setAllBots] = useState([]);
+  const [selectBot, setSelectBot] = useState(null);
 
   useEffect(() => {
-    fetchData();
+    fetchBots();
   }, []);
 
-  function enlistbot(bot) {
-    setBotsData(
-      botsData.map((r) => (r.id === bot.id ? { ...r, enlisted: true } : r))
-    );
-  }
+  const fetchBots = async () => {
+    try {
+      const response = await fetch('https://bot-battlr-data-0cjn.onrender.com/bots');
+      const data = await response.json();
+      const bots = data.map(bot => ({ ...bot, owned: false }));
+      setAllBots(bots);
+    } catch (error) {
+      console.error('Error fetching bots:', error);
+    }
+  };
 
-  function releasebot(bot) {
-    setbotsData(
-      botsData.map((r) => (r.id === bot.id ? { ...r, enlisted: false } : r))
-    );
-  }
+  const clickBot = (bot) => setSelectBot(bot);
 
-  function removebot(bot) {
-    const updatedBotsData = botsData.filter((r) => r.id !== bot.id);
-    setBotsData(updatedBotsData);
-  }
+  const handleInput = (event) => console.log("Input value:", event.target.value.toLowerCase());
+  
+  const clearSelection = () => setSelectBot(null);
 
   return (
-    <div>
-      <MyArmy
-        bots={botsData.filter((r) => r.enlisted)}
-        releaseBot={releaseBot}
-        removeBot={removeBot}
-      />
-      <BotCollection
-        bots={botsData}
-        enlistBot={enlistBot}
-        removeBot={removeBot}
-      />
+    <div className="container">
+      <BotSearch handleInput={handleInput} clearSelection={clearSelection} />
+      <br />
+      <MyBotArmy bots={allBots} selectBot={clickBot} />
+      <br />
+      <BotCollection bots={allBots} selectBot={clickBot} />
+      <br />
+      {selectBot && (
+        <BotSpecs bot={selectBot} clearSpec={clearSelection} addBot={() => {}} />
+      )}
     </div>
   );
-}
+};
 
 export default BotsPage;
+
 
